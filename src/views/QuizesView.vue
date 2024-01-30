1dea66961121755e731b3585317806b0431ccdba
Brent <brentmorine@gmail.com>
2024-01-29 22:59:56 -0400

    finished added some transitions for the styling and stuff

--- src/views/QuizesView.vue
+++ src/views/QuizesView.vue
@@ -2,13 +2,35 @@
     import q from '@/data/quizes.json'
     import { ref, watch } from 'vue'
     import Card from '@/components/Card.vue'
+    import gsap from "gsap"
 
     const quizes = ref(q)
     const search = ref("")
-
+    
     watch(search, () => {
       quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
     })
+
+    const afterEnter = (el) => {
+        console.log("after enter")
+    }
+
+    const beforeEnter = (el) => {
+        // card enter from
+        console.log('before enoter')
+        el.style.opacity = 0
+        el.style.transform = "translateY(-100px)"
+    }
+
+    const enter = (el) => {
+        gsap.to (el, {
+            y:0,
+            opacity: 1,
+            duration: 0.3,
+            delay: el.dataset.index * 0.3
+            
+        })
+    }
 
 </script>
 
@@ -19,7 +41,13 @@
       <input type="text" v-model.trim="search" placeholder="Search...">
     </header>
     <div class="options-container">
-      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+    <TransitionGroup appear
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter">
+      <Card v-for="(quiz, index) in quizes" :key="quiz.id" :quiz="quiz" :data-index="index"/>
+    </TransitionGroup>
+ 
       <!-- <div  v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="">
         <div class="card-text">
@@ -57,5 +85,7 @@
     flex-wrap: wrap;
     margin-top: 40px;
   }
+
+
 
 </style>
